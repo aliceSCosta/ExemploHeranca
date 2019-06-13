0daf557d00ba796c0a0d0e389dd39d4ea85c6d61
alicesilva <alicealmeyda72@gmail.com>
2019-06-13 15:18:30 -0300

    commit inicial Exemplo de relacionamento do tipo herança entre classes Início do desenvolvimento do menu principal e navegação entre telas

---
+++ ExemploHeranca/ExemploHeranca/FmrPrincipal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExemploHeranca
+{
+    public partial class FmrPrincipal : Form
+    {
+        public FmrPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void adcionarProdutoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FrmAdicionaProduto addProd = new FrmAdicionaProduto();
+            addProd.MdiParent = this;
+            addProd.WindowState = FormWindowState.Maximized;
+            addProd.Show();
+        }
+    }
+}
